d533a65797ca08194bc3b237de3c653ccdbcfad2
unknown <nandanskxworkz@gmail.com>
2022-05-19 22:48:32 +0530

    hello

---
+++ nestedfordemo.java
@@ -0,0 +1,48 @@
+/*class nestedfordemo
+{
+	
+public static void main(String[] args)
+{
+int n=2;
+int k=5;
+
+for(int i=1;i<=n;i++){
+for(int j=0;j<k;j++){
+
+System.out.print(i);
+}
+System.out.println();
+}
+}
+}*/
+
+
+
+
+
+class nestedfordemo
+{
+	
+public static void main(String[] args)
+{
+
+for(int l=1;l<=1;l++)
+{
+
+for(int m=1;m<5;m++)
+	
+{
+	System.out.print(m);
+	
+
+}
+System.out.print("\n");
+for(int n=5;n<9;n++)
+{
+
+System.out.print(n);
+}
+}
+}
+}
+

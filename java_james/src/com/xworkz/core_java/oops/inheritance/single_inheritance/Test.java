23f0ac959207b781e9890a660fba7a70eb71d06a
admin <nandanskxworkz@gmail.com>
2022-07-01 12:02:04 +0530

    inheritance

---
+++ java_james/src/com/xworkz/core_java/oops/inheritance/single_inheritance/Test.java
@@ -0,0 +1,10 @@
+package com.xworkz.core_java.oops.inheritance.single_inheritance;
+
+public class Test {
+	public static void main(String[] args) {
+		Soap soap=new Soap();
+		System.out.println(soap.getClass().getSimpleName());
+		
+	}
+
+}
